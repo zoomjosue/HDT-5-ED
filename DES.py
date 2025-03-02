cd30f88d5bd1dd85b7b55a66fcfb36215667b83e
ESJ0 <usuarioj0s3p1@gmail.com>
2025-03-02 17:55:03 -0600

    Se agrego la configuracion inicial que llevara la simulacion

---
+++ DES.py
@@ -0,0 +1,11 @@
+import simpy
+import random
+
+Semilla = 10
+random.seed(Semilla)
+CapacidadMemoria1 = 100
+CapacidadMemoria2 = 100
+CapacidadMemoria3 = 100
+VelocidadDeInstrucciones = 3
+Procesos = [25, 50, 100, 150, 200]
+Intervalos = [10, 5, 1]
